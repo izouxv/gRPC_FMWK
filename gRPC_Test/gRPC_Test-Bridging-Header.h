658443b8976fad2e5eb0fdafb507952ff522d356
zouxu <izouxv@gmail.com>
2016-04-29 10:48:09 +0800

    no message

--- gRPC_Test/gRPC_Test-Bridging-Header.h
+++ gRPC_Test/gRPC_Test-Bridging-Header.h
@@ -2,3 +2,19 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+
+
+
+#ifndef Fisher_Test_Bridging_Header_h
+#define Fisher_Test_Bridging_Header_h
+
+#import <GRPCClient/GRPCCall+Tests.h>
+#import "Helloworld.pbrpc.h"
+
+//#import <Fish/Helloworld.pbrpc.h>
+//#import <Fishproto/Helloworld.pbrpc.h>
+//
+//#import "NSObject+Util.h"
+//#import "NSObject+KVO.h"
+
+#endif
